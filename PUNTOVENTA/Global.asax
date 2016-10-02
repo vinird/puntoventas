5c0a8221b27946085de15aa34453660ee0226229
Vinicio <mvrd_17@hotmail.com>
2016-10-02 16:48:57 -0600

    Agregar archivos de proyecto.

---
+++ PUNTOVENTA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PUNTOVENTA.MvcApplication" Language="C#" %>
---
+++ PUNTOVENTA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PUNTOVENTA.Startup))]
+namespace PUNTOVENTA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
